a497bf6cf05ff9b43e603aee16e0d8cc9fdea9b6
Faye Billye <fbillye@student.21-school.ru>
2024-03-05 21:26:47 +0700

    Bug fix

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -1,7 +1,7 @@
 #include "client.h"
 
 int main(int argc, char *argv[]) {
-  if (argc < 3) {
+  if (argc != 3) {
     std::cerr << "Необходимо ввести адрес и порт сервера (например, "
                  "\"127.0.0.1\" и 8000)!"
               << std::endl;
